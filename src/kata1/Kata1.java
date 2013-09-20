155cc6aca606b10a193a0574138a4b35bc4c3274
usuario <usuario@10.22.146.205>
2013-09-20 13:57:24 +0100

    kata1 1era practica en el lab

---
+++ src/kata1/Kata1.java
@@ -0,0 +1,16 @@
+
+
+package kata1;
+
+import java.util.Date;
+
+
+public class Kata1 {
+
+ 
+    public static void main(String[] args) {
+       Student student = new Student ("jose", new Date (113, 9, 14 ));
+       System.out.println (student.getAge());
+       
+    }
+}
